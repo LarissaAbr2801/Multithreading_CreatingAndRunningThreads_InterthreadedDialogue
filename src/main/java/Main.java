09bd133d1014b444beb2b25f062695eb5d33fb0a
LarissaAbr2801 <abramova.la.28@gmail.com>
2022-05-18 13:18:26 +0300

    add constant

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
+    final static int TASKS_QUANTITY = 4;
+
     public static void main(String[] args) throws InterruptedException {
         ThreadGroup threadGroup = new ThreadGroup("main group");
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < TASKS_QUANTITY; i++) {
             workWithThreadInGroup(threadGroup, " " + (i + 1) + "");
         }
         Thread.sleep(15000);
